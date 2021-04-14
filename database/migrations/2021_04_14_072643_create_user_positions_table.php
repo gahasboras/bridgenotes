6248ef833f4c27d99fe539575a97bd9f3eb05bb2
gahasboras <harryyanto9@gmail.com>
2021-04-14 16:07:12 +0700

    init

---
+++ database/migrations/2021_04_14_072643_create_user_positions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserPositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_positions', function (Blueprint $table) {
+            $table->string('user_id')->nullable(false);
+            $table->string('status', 255)->nullable(true)->default('active');
+            $table->string('position', 255)->nullable(true);
+            $table->timestamps();
+            
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_positions');
+    }
+}
